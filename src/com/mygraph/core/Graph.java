9513cae670b9ffbfcabdf7f124bf5bdf22581816
Julien Simon <julien@julien.org>
2015-03-02 22:02:31 +0000

    Initial version

---
+++ src/com/mygraph/core/Graph.java
@@ -0,0 +1,92 @@
+package com.mygraph.core;
+
+import java.util.HashMap;
+
+public class Graph<T extends Vertex> {
+
+	private HashMap<T, VertexList<T>> vertices;
+	private boolean 				  isDirected;
+
+	public Graph(boolean isDirected) {
+		vertices = new HashMap<T, VertexList<T>>();
+		this.isDirected = isDirected;
+	}
+	
+	public boolean isDirected() {
+		return isDirected;
+	}
+	
+	public int countVertices() {
+		return vertices.size();
+	}
+	
+	public int countEdges() {
+		int count = 0;
+		for (VertexList<T> v : vertices.values()) {
+			count += v.countEdges();
+		}
+		if (!isDirected()) {
+			count /= 2;
+		}
+		return count;
+	}
+			
+	// This method adds an edge to the graph
+	// It will add the source and destination vertexes if needed
+	// if the edge already exists, it won't be duplicated
+	public boolean addEdge(T source, T destination, int weight) {
+		if ((source == null) || (destination == null)) {
+			return false;
+		}
+		// Add vertexes if they're not already in the graph
+		if (!vertices.containsKey(source)) {
+			vertices.put(source, new VertexList<T>(source.getId()));
+		}
+		if (!vertices.containsKey(destination)) {
+			vertices.put(destination, new VertexList<T>(destination.getId()));
+		}
+		// Add edge
+		vertices.get(source).addEdge(destination, weight);
+		// If the graph is not directed, we need to add the reciprocal edge to the destination
+		if (!isDirected()) {
+			vertices.get(destination).addEdge(source, weight);
+		}
+		return true;
+	}
+	
+	public boolean addEdge(T source, T destination) {
+		return addEdge(source, destination, 1);
+	}
+
+	public boolean removeEdge(T source, T destination) {
+		if ((source == null) || (destination == null)) {
+			return false;
+		}
+		vertices.get(source).removeEdge(destination);
+		if (!isDirected()) {
+			vertices.get(destination).removeEdge(source);
+		}
+		return true;
+	}
+	
+	public boolean hasEdge(T source, T destination, int weight) {
+		if ((source == null) || (destination == null)) {
+			return false;
+		}
+		return vertices.get(source).hasEdgeTo(destination, weight);
+	}
+
+	public boolean hasEdge(T source, T destination) {
+		if ((source == null) || (destination == null)) {
+			return false;
+		}
+		return vertices.get(source).hasEdgeTo(destination);
+	}
+	
+	public void display() {
+		System.out.println("Vertices: "+countVertices()+", Edges: "+countEdges());
+		for (VertexList<T> v : vertices.values()) {
+			System.out.println(v);
+		}
+	}	
+}
---
+++ src/com/mygraph/core/VertexList.java
@@ -0,0 +1,74 @@
+package com.mygraph.core;
+
+import java.util.HashMap;
+
+public class VertexList<T extends Vertex> {
+	
+	private int 				sourceId;
+	private HashMap<T, Integer> edges;
+	
+	VertexList(int sourceId) {
+		this.sourceId = sourceId;
+		this.edges = new HashMap<T, Integer>();
+	}
+	
+	int countEdges() {
+		return edges.size();
+	}
+	
+	boolean addEdge(T destination, int weight) {
+		if (destination == null) {
+			return false;
+		}
+		if (edges.get(destination) != null) {
+			return false;
+		}
+		edges.put(destination, weight);
+		return true;		
+	}
+		
+	boolean removeEdge(T destination) {
+		if (destination == null) {
+			return false;
+		}
+		return (edges.remove(destination) != null);
+	}
+	
+	boolean hasEdgeTo(T destination) {
+		if (destination == null) {
+			return false;
+		}
+		return (edges.get(destination) != null);
+	}
+	
+	boolean hasEdgeTo(T destination, int weight) {
+		if (destination == null) {
+			return false;
+		}
+		Integer i = edges.get(destination);
+		if (i != null) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	int isWeight(T destination) {
+		if (destination == null) {
+			return 0;
+		}
+		Integer i = edges.get(destination);
+		if (i != null) {
+			return i;
+		}
+		else {
+			return 0;
+		}
+	}
+	
+	public String toString() {
+		return "("+sourceId+")->"+edges;
+	}
+}
+
