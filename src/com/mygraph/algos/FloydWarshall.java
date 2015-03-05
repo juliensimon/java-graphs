0ece6da8fa1f64babba1e8615da1342b8ce6eda3
Julien Simon <julien@julien.org>
2015-03-05 22:00:06 +0100

    Initial revision

---
+++ src/com/mygraph/algos/FloydWarshall.java
@@ -0,0 +1,91 @@
+package com.mygraph.algos;
+
+import java.util.ArrayList;
+
+import com.mygraph.core.Graph;
+import com.mygraph.core.Vertex;
+
+public class FloydWarshall<T extends Vertex> {
+
+	private Graph<T> g;
+	private int 	 size;
+	private long[][] distance;
+	private int[][]	 predecessor;
+
+	
+	FloydWarshall(Graph<T> g) {
+		this.g 		= g;
+		size 		= g.countVertices();
+		distance 	= new long[size+1][size+1];	
+		predecessor = new int[size+1][size+1];	
+	}
+	
+	private void init() {
+		for (int i=1;i<=size;i++) {
+			for (int j=1;j<=size;j++) {
+				distance[i][j] = Integer.MAX_VALUE;
+				predecessor[i][j] = -1;
+			}
+			distance[i][i] = 0;
+		}
+		
+		for (T v : g.getVertices()) {
+			for (T w : g.getNeighbors(v)) {
+				distance[v.getId()][w.getId()] = g.isWeight(v, w);
+				predecessor[v.getId()][w.getId()] = v.getId();
+			}
+		}		
+	}
+	
+	private void printDistance() {
+		for (int i=1;i<=size;i++) {
+			for (int j=1;j<=size;j++) {
+				System.out.print(distance[i][j]+ "\t");
+			}
+			System.out.println();
+		}
+	}
+	
+	public ArrayList<Integer> findShortestPath(T source, T destination) {
+		if ( (source == null) || (destination == null)) {
+			return null;
+		}
+
+		int srcId = source.getId();
+		int dstId = destination.getId();
+		// If the destination has no predecessor, there is no path to it
+		if (predecessor[srcId][dstId] == -1) {
+			return null;
+		}
+		// Walk through the predecessors and add them at the front of the list
+		ArrayList<Integer> path = new ArrayList<Integer>();
+		while (dstId != -1) {
+			path.add(0, dstId);
+			dstId = predecessor[srcId][dstId];
+		}	
+		// The list contains the path from the start vertex to the destination vertex
+		return path;
+	}
+	
+	public void search() {
+		
+		init();
+		
+		for (T u : g.getVertices()) {
+			for (T v : g.getVertices()) {
+				for (T w : g.getVertices()) {
+
+					//printDistance();
+					long newLength = distance[v.getId()][u.getId()] + distance[u.getId()][w.getId()];
+					//System.out.println("\nTrying "+v+","+u+" and "+u+","+w+" : dist="+newLength);
+
+					if (newLength < distance[v.getId()][w.getId()]) {
+						//System.out.println(" --> route is shorter");
+						distance[v.getId()][w.getId()] = newLength;
+						predecessor[v.getId()][w.getId()] = predecessor[u.getId()][w.getId()];
+					}
+				}
+			}
+		}
+	}
+}
---
+++ src/com/mygraph/algos/TestFloydWarshall.java
@@ -0,0 +1,70 @@
+package com.mygraph.algos;
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.mygraph.core.Graph;
+import com.mygraph.core.Vertex;
+
+public class TestFloydWarshall {
+
+	Vertex v1, v2, v3, v4, v5, v6, v7;
+	Graph<Vertex> g;
+	FloydWarshall<Vertex> fw;
+	ArrayList<Integer> path;
+	
+	@Before
+	public void setUp() throws Exception {
+		v1 = new Vertex(1);
+		v2 = new Vertex(2);
+		v3 = new Vertex(3);
+		v4 = new Vertex(4);
+		v5 = new Vertex(5);
+		v6 = new Vertex(6);
+		v7 = new Vertex(7);
+
+		g = new Graph<Vertex>(true);
+		g.addEdge(v1, v2, 2);
+		g.addEdge(v1, v3, 1);
+		g.addEdge(v2, v3, 2);
+		g.addEdge(v2, v5, 1);
+		g.addEdge(v2, v6, 1);
+		g.addEdge(v3, v4, 1);
+		g.addEdge(v3, v5, 3);
+		g.addEdge(v4, v7, 3);
+		g.addEdge(v5, v7, 2);
+		
+		fw = new FloydWarshall<Vertex>(g);
+		fw.search();
+	}
+
+	@Test
+	public void test1() {
+		path = fw.findShortestPath(v1, v7);
+		assertEquals(path.size(), 4);
+		assertEquals(path.get(0), (Integer)1);
+		assertEquals(path.get(1), (Integer)2);
+		assertEquals(path.get(2), (Integer)5);
+		assertEquals(path.get(3), (Integer)7);
+	}
+	
+	@Test
+	public void test2() {
+		path = fw.findShortestPath(v2, v7);
+		assertEquals(path.size(), 3);
+		assertEquals(path.get(0), (Integer)2);
+		assertEquals(path.get(1), (Integer)5);
+		assertEquals(path.get(2), (Integer)7);
+	}
+	
+	@Test
+	public void test3() {
+		path = fw.findShortestPath(v7, v1);
+		assertEquals(path, null);
+	}
+
+}
