1d4e8029ea3bf86f4ce55153e8fa63d4ebeefe47
Julien Simon <julien@julien.org>
2015-03-26 14:14:45 +0100

    Add Prim’s algorithm (Minimum Spanning Tree) + basic test.

---
+++ src/com/mygraph/algos/Prim.java
@@ -0,0 +1,84 @@
+package com.mygraph.algos;
+
+import java.util.Iterator;
+import java.util.PriorityQueue;
+
+import com.mygraph.core.Graph;
+import com.mygraph.core.Vertex;
+
+//As per "Algorithms in a Nutshell", O'Reilly, 2009
+
+//
+//Find the minimum spanning tree
+//Best case o((v+e)*log(v)), median case o((v+e)*log(v)), worst case o((v+e)*log(v))
+//
+
+//This implementation uses a PriorityQueue.
+
+public class Prim<T extends Vertex> {
+	
+	private final Graph<T> g;
+	private final int size;
+	private final int[] weight;
+	private final int[] predecessor;
+
+	Prim(Graph<T> g) {
+		this.g = g;
+		size = g.countVertices();
+		weight = new int[size + 1];
+		predecessor = new int[size + 1];
+	}
+	
+	public void minSpanningTree(T start) {
+		// Start with infinite distances between the start vertex and all others
+		for (int i = 1; i < (this.size + 1); i++) {
+			weight[i] = Integer.MAX_VALUE;
+			predecessor[i] = -1;
+		}
+		weight[start.getId()] = 0;
+
+		// Build a list of (vertex, distance) couples
+		PriorityQueue<QueueElement<T>> pq = 
+				new PriorityQueue<QueueElement<T>>(new QueueElementMinFirst<T>());
+		for (T v : g.getVertices()) {
+			pq.add(new QueueElement<T>(v, weight[v.getId()]));
+		}
+
+		while (pq.size() != 0) {
+
+			// Get and remove the vertex that is the closest to the start node
+			// (aka the min node)
+			// It's the first element of the queue
+			T min = pq.poll().getVertex();
+			assert(min != null);
+
+			// For each neighbor
+			for (T neighbor : g.getNeighbors(min)) {				
+				// Find the neighbor in the queue
+				Iterator<QueueElement<T>> i = pq.iterator();
+				while (i.hasNext()) {
+					QueueElement<T> e = i.next();
+					if (e.getVertex().equals(neighbor)) {
+						int w = g.isWeight(min, neighbor);
+						// If this path is shorter, remember it
+						if (w < weight[neighbor.getId()]) {
+							predecessor[neighbor.getId()] = min.getId();
+							weight[neighbor.getId()] = w;						
+							// Update its distance to the start vertex
+							e.setDistance(w);
+							// Remove and add again to maintain queue order
+							pq.remove(e);
+							pq.add(e);
+							break;
+						}
+					}
+				}
+			}
+		}
+		
+		
+	  for (int i=1; i<=this.size;i++) { System.out.println("pred["+i+"]"+"="+predecessor[i]); }
+	  for (int i=1; i<=this.size;i++) { System.out.println("weight["+i+"]"+"="+weight[i]); }
+
+	}
+}
---
+++ src/com/mygraph/algos/TestPrim.java
@@ -0,0 +1,48 @@
+package com.mygraph.algos;
+
+
+import java.util.ArrayList;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.mygraph.core.Graph;
+import com.mygraph.core.Vertex;
+
+public class TestPrim {
+
+	Vertex v1, v2, v3, v4, v5, v6, v7;
+	Graph<Vertex> g;
+	Prim<Vertex> p;
+	ArrayList<Integer> path;
+
+	@Before
+	public void setUp() throws Exception {
+		v1 = new Vertex(1);
+		v2 = new Vertex(2);
+		v3 = new Vertex(3);
+		v4 = new Vertex(4);
+		v5 = new Vertex(5);
+		v6 = new Vertex(6);
+		v7 = new Vertex(7);
+
+		g = new Graph<Vertex>(true);
+		g.addEdge(v1, v2, 2);
+		g.addEdge(v1, v3, 1);
+		g.addEdge(v2, v3, 2);
+		g.addEdge(v2, v5, 1);
+		g.addEdge(v2, v6, 1);
+		g.addEdge(v3, v4, 1);
+		g.addEdge(v3, v5, 3);
+		g.addEdge(v4, v7, 3);
+		g.addEdge(v5, v7, 2);
+	}
+
+	@Test
+	public void test() {
+		Prim<Vertex> p = new Prim<Vertex>(g);
+
+		p.minSpanningTree(v1);
+	}
+}
